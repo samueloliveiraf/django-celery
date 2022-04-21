58adf4291fecc05dad4d461e7dfd53bf97092e80
samueloliveiraf <samueldiablo73@gmail.com>
2022-04-21 18:52:33 -0300

    add

--- apps/produtos/views.py
+++ apps/produtos/views.py
@@ -1,5 +1,7 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
-    ListView
+    ListView,
+    UpdateView
 )
 from .models import Produto
 
@@ -7,3 +9,17 @@ from .models import Produto
 class ListViewProduct(ListView):
     template_name = 'home.html'
     model = Produto
+
+
+class UpdateViewProduct(UpdateView):
+    template_name = 'edit.html'
+    model = Produto
+
+    fields = [
+        'nome',
+        'quantidade',
+        'ativo'
+    ]
+
+    success_url = reverse_lazy('home')
+
--- django_celery/urls.py
+++ django_celery/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from apps.core.views import home
+from apps.produtos.views import UpdateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('editar/<int:pk>/', UpdateView.as_view(), name='editar')
 ]
