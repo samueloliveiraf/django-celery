12cd6ea73a223476600b366ac0815bb017e33142
samueloliveiraf <samueldiablo73@gmail.com>
2022-04-21 15:13:32 -0300

    add

--- django_celery/settings.py
+++ django_celery/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['140.82.43.23', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['140.82.43.23', 'localhost',  '127.0.0.1']
 
 # Application definition
 
@@ -131,10 +132,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
