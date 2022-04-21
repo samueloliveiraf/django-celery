9d69c6e1302c23e725d9e6167d6b1ec7cb8fa1f3
samueloliveiraf <samueldiablo73@gmail.com>
2022-04-21 15:17:15 -0300

    add

--- django_celery/settings.py
+++ django_celery/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
