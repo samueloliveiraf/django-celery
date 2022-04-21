103ef23fafabd519d7f4d1dfc9ed9ba1e4097e77
samueloliveiraf <samueldiablo73@gmail.com>
2022-04-21 18:58:26 -0300

    add

--- django_celery/urls.py
+++ django_celery/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from apps.core.views import home
-from apps.produtos.views import UpdateView
+from apps.produtos.views import UpdateViewProduct
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('editar/<int:pk>/', UpdateView.as_view(), name='editar')
+    path('editar/<int:pk>/', UpdateViewProduct.as_view(), name='editar')
 ]
